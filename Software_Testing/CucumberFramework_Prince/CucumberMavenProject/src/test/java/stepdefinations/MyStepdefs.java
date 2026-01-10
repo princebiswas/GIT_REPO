c49b598589fa2679094b0a3cbd2f8a7fcab4816e
princebiswas <princebiswas@live.com>
2026-01-10 02:14:05 -0500

    changes

--- Software_Testing/CucumberFramework_Prince/CucumberMavenProject/src/test/java/stepdefinations/MyStepdefs.java
+++
@@ -1,8 +0,0 @@
-public class MyStepdefs {
-    public MyStepdefs() {
-        Given("^I navigate to the Ebay UI$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new io.cucumber.java.PendingException();
-        });
-    }
-}
